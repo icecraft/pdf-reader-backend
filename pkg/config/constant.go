40fad01493184735aaba8c9871553cd41fcbcb2b
xu rui <xurui1@pjlab.org.cn>
2023-02-09 20:19:45 +0800

    elasticsearch schema

--- pkg/config/constant.go
+++ pkg/config/constant.go
@@ -10,4 +10,16 @@ const (
 	ES_ENDPOINTS = "ES_ENDPOINTS"
 	ES_USERNAME  = "ES_USERNAME"
 	ES_PASSWORD  = "ES_PASSWORD"
+
+	ES_Mapping = `{
+		"mappings": {
+		  "properties": {
+			"word":    { "type": "keyword" },  
+			"en":    {"type": "text"},
+			"cn":    {"type": "text"},
+			"examples":  {"type": "text"}, 
+			"hits" : {"type": "date"}
+		  }
+		}
+	  }`
 )
