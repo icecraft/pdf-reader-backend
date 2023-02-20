9afc5a390995b072688cbc8d1a473c23be8d1a5f
xu rui <xurui1@pjlab.org.cn>
2023-02-20 18:40:41 +0800

    daily commit

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -5,29 +5,28 @@ import (
 
 	"gitlab.shlab.tech/xurui/pdf-reader-backend/pkg/config"
 	"gitlab.shlab.tech/xurui/pdf-reader-backend/pkg/log"
-	"gitlab.shlab.tech/xurui/pdf-reader-backend/pkg/utils"
 	"gitlab.shlab.tech/xurui/pdf-reader-backend/pkg/svc"
-
+	"gitlab.shlab.tech/xurui/pdf-reader-backend/pkg/utils"
 )
 
 type Server struct {
 	conf     *config.Config
 	esClient *es.Client
-	ciba    *svc.Ciba
+	ciba     *svc.Ciba
 }
 
 func NewServer(conf *config.Config, logFormat string) (*Server, error) {
 	var s Server
 	s.conf = conf
 
-	esClient, err := utils.InitEs(conf.ES)
+	esClient, err := utils.InitEs(conf.ES, config.ES_Mapping, config.CibaIndexName)
 	if err != nil {
 		log.Error(err, "failed to init es", "conf", conf.ES)
 		return nil, err
 	}
 	s.esClient = esClient
 
-	s.ciba = &svc.Ciba{} 
-	
+	s.ciba = &svc.Ciba{}
+
 	return &s, nil
 }
--- pkg/utils/es.go
+++ pkg/utils/es.go
@@ -8,14 +8,33 @@ import (
 	"gitlab.shlab.tech/xurui/pdf-reader-backend/pkg/config"
 )
 
-func InitEs(conf config.ESConf) (*es.Client, error) {
+func InitEs(conf config.ESConf, mapping, indexName string) (*es.Client, error) {
 	var options []es.ClientOptionFunc
 	options = append(options, es.SetURL(conf.Url...))
 	options = append(options, es.SetBasicAuth(conf.Username, conf.Password))
 	options = append(options, es.SetSniff(true))
 	// options = append(options, es.SetTraceLog(eslog.New(os.Stdout, "", 0)))  //debug
 
-	return es.NewClient(options...)
+	client, err := es.NewClient(options...)
+	if err != nil {
+		return nil, err
+	}
+	if len(mapping) != 0 && len(indexName) > 0 {
+		exists, err := client.IndexExists(indexName).Do(context.TODO())
+
+		if err != nil {
+			return nil, err
+		}
+
+		if !exists {
+			_, err = client.CreateIndex(indexName).Body(mapping).Do(context.TODO())
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return client, nil
 }
 
 func ExistedIndex(ctx context.Context, client *es.Client, index string) (bool, error) {
