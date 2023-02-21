ea14b9141469ce418583879c3443032d0c94c79f
xu rui <xurui1@pjlab.org.cn>
2023-02-21 13:52:15 +0800

    update dev env

--- resources/init-local-dev-env.sh
+++ resources/init-local-dev-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_DATA=$HOME/Public/shared_docker_data/pdf_reader
+DOCKER_DATA=$HOME/.dev/shared_docker_data/pdf_reader
 
 mkdir -p $DOCKER_DATA
 mkdir -p $DOCKER_DATA/elasticsearch
